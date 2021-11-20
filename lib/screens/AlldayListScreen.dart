9ab13b26b819e4f85da2bd8366dcdc96f64981cd
Hide Toyoda <hide.toyoda@gmail.com>
2021-11-20 23:37:20 +0900

    all screen graph create

--- lib/screens/AlldayListScreen.dart
+++ lib/screens/AlldayListScreen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: unnecessary_null_comparison, file_names, import_of_legacy_library_into_null_safe, prefer_final_fields
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../utilities/utility.dart';
 import '../utilities/CustomShapeClipper.dart';
@@ -35,10 +37,17 @@ class _AlldayListScreenState extends State<AlldayListScreen> {
 
   List<Map<dynamic, dynamic>> _ymList = [];
 
+  late TrackballBehavior _trackballBehavior;
+
   /// 初期動作
   @override
   void initState() {
     super.initState();
+
+    _trackballBehavior = TrackballBehavior(
+      enable: true,
+      activationMode: ActivationMode.singleTap,
+    );
 
     _makeDefaultDisplayData();
   }
@@ -197,7 +206,9 @@ class _AlldayListScreenState extends State<AlldayListScreen> {
   Widget _dispPageContent() {
     return Column(
       children: <Widget>[
+        _makeGraph(),
         Container(
+          width: double.infinity,
           decoration: BoxDecoration(color: Colors.white.withOpacity(0.3)),
           child: Wrap(
             children: _makeYmBtn(),
@@ -222,44 +233,50 @@ class _AlldayListScreenState extends State<AlldayListScreen> {
   Widget _listItem({required int position}) {
     _utility.makeYMDYData(_alldayData[position]['date'], 0);
 
-    return Card(
-      color: _utility.getBgColor(_alldayData[position]['date'], _holidayList),
-      elevation: 10.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
+    return Container(
+      margin: EdgeInsets.only(bottom: 5),
+      decoration: BoxDecoration(
+        color: _utility.getBgColor(_alldayData[position]['date'], _holidayList),
       ),
-      child: ListTile(
-        title: DefaultTextStyle(
-          style: const TextStyle(fontSize: 10.0),
-          child: Table(
-            children: [
-              TableRow(children: [
-                Text('${_alldayData[position]['date']}（${_utility.youbiStr}）'),
-                Container(
-                  alignment: Alignment.topRight,
-                  child: Text(_utility
-                      .makeCurrencyDisplay(_alldayData[position]['total'])),
-                ),
-                Container(
-                  alignment: Alignment.topRight,
-                  child: Text(_utility
-                      .makeCurrencyDisplay(_alldayData[position]['diff'])),
-                ),
-              ]),
-            ],
-          ),
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: DefaultTextStyle(
+        style: TextStyle(fontSize: 12),
+        child: Row(
+          children: [
+            Container(
+              width: 120,
+              child: Text(
+                  '${_alldayData[position]['date']}（${_utility.youbiStr}）'),
+            ),
+            Container(
+              width: 60,
+              alignment: Alignment.topRight,
+              child: Text(
+                  _utility.makeCurrencyDisplay(_alldayData[position]['total'])),
+            ),
+            Container(
+              width: 100,
+              alignment: Alignment.topRight,
+              child: Text(
+                  _utility.makeCurrencyDisplay(_alldayData[position]['diff'])),
+            ),
+            Container(
+              width: 50,
+              alignment: Alignment.center,
+              child: (_alldayData[position]['zeroflag'] == 1)
+                  ? Icon(
+                      Icons.star,
+                      color: Colors.greenAccent.withOpacity(0.5),
+                      size: 10,
+                    )
+                  : const Icon(
+                      Icons.check_box_outline_blank,
+                      color: Color(0xFF2e2e2e),
+                      size: 10,
+                    ),
+            ),
+          ],
         ),
-        trailing: (_alldayData[position]['zeroflag'] == 1)
-            ? Icon(
-                Icons.star,
-                color: Colors.greenAccent.withOpacity(0.5),
-                size: 10,
-              )
-            : const Icon(
-                Icons.check_box_outline_blank,
-                color: Color(0xFF2e2e2e),
-                size: 10,
-              ),
       ),
     );
   }
@@ -282,8 +299,8 @@ class _AlldayListScreenState extends State<AlldayListScreen> {
           onTap: () => _scroll(pos: _ymList[i]['pos']),
           child: Container(
             color: Colors.green[900]!.withOpacity(0.5),
-            margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 4),
             child: Text(
               '${_ymList[i]['ym']}',
               style: const TextStyle(fontSize: 10),
@@ -293,6 +310,68 @@ class _AlldayListScreenState extends State<AlldayListScreen> {
       );
     }
     return _btnList;
+  }
+
+  Widget _makeGraph() {
+    List<ChartData> _list = [];
+
+    _utility.makeYMDYData(DateTime.now().toString(), 0);
+    var thisYear = _utility.year;
+
+    int minimumTotal = 0;
+    for (var i = 0; i < _alldayData.length; i++) {
+      _utility.makeYMDYData(_alldayData[i]['date'], 0);
+
+      if (thisYear != _utility.year) {
+        continue;
+      }
+
+      if (minimumTotal == 0) {
+        if (minimumTotal < int.parse(_alldayData[i]['total'])) {
+          minimumTotal = int.parse(_alldayData[i]['total']);
+        }
+      }
+
+      _list.add(
+        ChartData(
+          x: DateTime(
+            int.parse(_utility.year),
+            int.parse(_utility.month),
+            int.parse(_utility.day),
+          ),
+          total: int.parse(_alldayData[i]['total']),
+        ),
+      );
+    }
+
+    var devide1000000 = (minimumTotal / 1000000).floor();
+
+    return Container(
+      height: 250,
+      child: SfCartesianChart(
+        title: ChartTitle(text: thisYear),
+        trackballBehavior: _trackballBehavior,
+        series: <ChartSeries>[
+          LineSeries<ChartData, DateTime>(
+            color: Colors.yellowAccent,
+            dataSource: _list,
+            xValueMapper: (ChartData data, _) => data.x,
+            yValueMapper: (ChartData data, _) => data.total,
+          ),
+        ],
+        primaryXAxis: DateTimeAxis(
+          majorGridLines: MajorGridLines(width: 0),
+          dateFormat: DateFormat.MMM(),
+        ),
+        primaryYAxis: NumericAxis(
+          majorGridLines: MajorGridLines(
+            width: 2,
+            color: Colors.white,
+          ),
+          minimum: (devide1000000 * 1000000),
+        ),
+      ),
+    );
   }
 
 ////////////////////////////////////////////////////////
@@ -308,4 +387,14 @@ class _AlldayListScreenState extends State<AlldayListScreen> {
       ),
     );
   }
+}
+
+class ChartData {
+  final DateTime x;
+  final num total;
+
+  ChartData({
+    required this.x,
+    required this.total,
+  });
 }
